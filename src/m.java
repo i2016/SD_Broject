728ccd8cb8b6858f8c98b7b3f88fb6b6962c8927
islam <islam>
2016-05-07 21:43:54 +0200

    infix to postfix function

--- src/m.java
+++ src/m.java
@@ -7,7 +7,42 @@ import java.util.*;
 
 public class m {
 	
-
+	static Stack list = new Stack();
+	///////////////////////////» ‘Ê› Â· œÂ —ﬁ„ Ê·« ⁄·«„…///////////////////////// 
+	public static <E> boolean isOperand(E ch)
+	{
+	 if(ch.equals('.')||ch.equals('0')||ch.equals('1')||ch.equals('2')||ch.equals('3')||ch.equals('4')||ch.equals('5')||ch.equals('6')||ch.equals('7')||ch.equals('8')||ch.equals('9')||ch.equals('#'))
+	 return true;
+	 else 
+		 return false;
+	 
+	}
+///////////////////////////» ‘Ê› Â· œÂ —ﬁ„ Ê·« ⁄·«„…///////////////////////// 
+	public static<E> boolean isOperator(E ch)
+	{
+	 if(ch.equals('+')||ch.equals('-')||ch.equals('/')||ch.equals('*')||ch.equals('^')||ch.equals('%'))
+	 return true;
+	 else 
+		 return false;
+	 
+	}
+	///////////////////» Õœœ «·«Ê·ÊÌ… ///////////////////////
+	public static<E> int prority(E ch)
+	{
+		
+		switch ((Character)ch)
+		{
+		
+		case'+':case'-': return 1;
+		
+		case'*':case'/':case'%': return 2;
+		
+		case'^':return 3;
+		
+		default :return 0;
+		
+		}
+	}
 		
 ///////////////////////////» ÕÊ· „‰ infix «·Ï postfix///////////////////////// 
 	public static double toPost(String inp){
@@ -66,79 +101,6 @@ public class m {
 		return evalPost(result);//evalPost(result.split(" "));
 	
 	}
-
-	
-	public static double evalPost(String exp)
-	{
-	   double res = 0;
-
-    
-    double n1;     //result of 1st popping
-    double n2;     // result of 2nd popping
-    char ch = 0 ;
-String number="";
-    for (int i = 1; i <= exp.length(); i++) {
-         ch = exp.charAt(i-1);
-
-
-            if (isOperand(ch)) {
-            	if(isOperand(exp.charAt(i)))
-            	{
-            		number+=ch;
-           		}
-            	else 
-            	{
-            		number+=ch;
-            		String s= number.replace("#", "-");
-            		list.push (Double.parseDouble(s));
-            		number="";
-            		s="";
-            	}
-            		
-            	
-            }
-             
-    else if (isOperator(ch)) 
-            
-            	 {
-                     n1 = Double.parseDouble(("" + list.pop()));
-                     n2 = Double.parseDouble(("" + list.pop()));
-
-                     switch (ch) {
-                         case '+':
-                             list.push(n1 + n2);
-                             break;
-                         case '-':
-                             list.push(n2 - n1);
-                             break;
-                         case '*':
-                             list.push(n1 * n2);
-                             break;
-                         case '/':
-                             try{
-                            	 
-                        	 list.push(n2 / n1);
-                             }catch(Exception ex)
-                             {
-                            	
-                             }
-                             
-                             break;
-
-                         default:
-                             System.out.println("Invalid operator order!");
-                     }  
-            	 }
-    else
-    {}
-    }
-            
-        
-
-    res = Double.parseDouble(("" + list.pop()));
-
-    return res;
-}	
 
  
 
