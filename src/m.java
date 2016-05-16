bac8842cf447fd0fd085d76f3a7a34d20247a462
i2016 <islam.kamal.fci@gmail.com>
2016-05-16 16:27:03 +0200

    Update m.java

--- src/m.java
+++ src/m.java
@@ -1,10 +1,13 @@
+package com.Calc;
 
-package javafxapplication1;
+
+
 import java.util.Stack;
 public class Operation 
 {
-    static Stack list = new Stack();
-	// detremine if character is number or not 
+    static Stack<Double> list = new Stack<Double>();// use to evaluation postfix Expression
+    
+	// Determine if character is number or not 
 public static <E> boolean isOperand(E ch)
 	{
 	 if(ch.equals('.')||ch.equals('0')||ch.equals('1')||ch.equals('2')||ch.equals('3')||ch.equals('4')||ch.equals('5')||ch.equals('6')||ch.equals('7')||ch.equals('8')||ch.equals('9')||ch.equals('#'))
@@ -13,7 +16,7 @@ public static <E> boolean isOperand(E ch)
          return false;
 	 
 	}
-// detremine if character is Operator or not  
+// Determine if character is Operator or not  
 public static <E> boolean isOperator(E ch)
 	{
           if(ch.equals('=')||ch.equals('+')||ch.equals('-')||ch.equals('/')||ch.equals('*')||ch.equals('^')||ch.equals('%')||ch.equals('√')||ch.equals('!')||ch.equals('l')||ch.equals('g'))
@@ -21,7 +24,7 @@ public static <E> boolean isOperator(E ch)
 	 else 
          return false;
 	 }
-	// detremine the prority of operator
+	// Determine the priority of operator
 public static <E> int prority(E ch)
 	{
 		
@@ -36,15 +39,16 @@ public static <E> int prority(E ch)
 		default :return 0;
 		}
 	}
+
+
 public static String toPost(String inp) throws Exception
        {
 		String result="";       //Create Empty Postfix String
 		String copy='('+inp+')';  //Given Infix To Store String
 		Stack<Character> operator=new Stack<>();/*Create Generic Stack To Store 
-                Operator And Organize Priority Of Operatores */
-		int size=copy.length();
+                Operator And Organize Priority Of Operators */
+		int size=copy.length();//size of operation
 		char ch,chh;
-		boolean b=false;
 		for(int i=1;i<size;i++)
 		{                             /* if ch is an operand then
                                               append it to the output postfix string */
@@ -52,22 +56,22 @@ public static String toPost(String inp) throws Exception
 			ch=copy.charAt(i-1);
 			chh=copy.charAt(i);
 			if(isOperand(ch))
-			{                             /*if ch is an operator then
+			{                             /*if ch is an operand then
                                               append it to the output postfix string */
-				if(isOperator(chh)||chh==')'||chh==')')
-				result+=ch+" ";
-				else result+=ch;
+				if(isOperator(chh)||chh==')'||chh==')')//to determined the full number and the end of number
+				result+=ch+" ";  //add the number flowing by space to separate the elements
+				else result+=ch;//if number still complete it
 			}
 		
-                        else if(isOperator(ch))
+                        else if(isOperator(ch))   //if element is operator then check if :
                         {  
-				if(!operator.empty())
+				if(!operator.empty())       //the stack is not empty
 					{
 					while(!operator.empty())  
-                               /* while operator stack not emptypop operator
+                               /* while operator stack not empty ,pop operator
                                     append it to the postfix string*/
                                      {        
-						if(prority(ch)>prority((Character)operator.peek()))
+						if(prority(ch)>prority((Character)operator.peek())) //determined the priority of operand
 								{
 								operator.push(ch);
 								break;
@@ -81,7 +85,7 @@ public static String toPost(String inp) throws Exception
 			}
 			else if(ch=='('){
 				operator.push(ch);}
-			else if(ch==')')
+			else if(ch==')')//end of braces 
 			{
 				while((Character)operator.peek()!='(')
 				{
@@ -92,19 +96,24 @@ public static String toPost(String inp) throws Exception
 			
 			}
 			while(operator.size()>1)
-				result+=operator.pop();
+				result+=operator.pop();       // to make sure that no thing in stack
 			
-		return result(evalPost(result));//evalPost(result.split(" "));
+		return result(evalPost(result));//evalPost(result.split(" ")); 
 	}
+
+
+
+
+
+//compute the postfix expression 
 	public static double evalPost(String exp) throws Exception
 	{
-	   double res = 0;
+	   double res = 0;//final result
 
-    
     double n1;     //result of 1st popping
     double n2;     // result of 2nd popping
     char ch = 0 ;
-String number="";
+    String number="";
     for (int i = 1; i <= exp.length(); i++) {
          ch = exp.charAt(i-1);
 
@@ -112,12 +121,12 @@ String number="";
             if (isOperand(ch)) {
             	if(isOperand(exp.charAt(i)))
             	{
-            		number+=ch;
+            		number+=ch;//collect full number
            		}
             	else 
             	{
             		number+=ch;
-            		String s= number.replace("#", "-");
+            		String s= number.replace("#", "-");//replace the # by minus if found 
             		list.push (Double.parseDouble(s));
             		number="";
             		s="";
@@ -126,9 +135,9 @@ String number="";
             	
             }
              
-    else if (isOperator(ch)) 
+    else if (isOperator(ch)) //if operator do the operation on number according to the kind of it
             
-            	 {   if(ch=='!'||ch=='√'||ch=='l'||ch=='g')
+            	 {   if(ch=='!'||ch=='√'||ch=='l'||ch=='g') //operations that have single pop number
                  {
     	    n1 = Double.parseDouble(("" + list.pop()));
                     switch (ch) {  
@@ -159,29 +168,30 @@ String number="";
                    
             	 else
             		{
+            		 //operations that have two pop number
             		 n1 = Double.parseDouble(("" + list.pop()));
                           n2 = Double.parseDouble(("" + list.pop()));
                      switch (ch) 
                      {
                          case '+':
-                             list.push(n1 + n2);
+                             list.push(n1 + n2);//adding
                              break;
                          case '-':
-                             list.push(n2 - n1);
+                             list.push(n2 - n1);//Subtracting
                              break;
                          case '*':
-                             list.push(n1 * n2);
+                             list.push(n1 * n2);//Multiply
                              break;
                          case '/':
                              try{
                             	 
-                        	 list.push(n2 / n1);
+                        	 list.push(n2 / n1);//Division
                              }catch(Exception ex)
                              {
                                                          
                              }   break;
                          case '^':
-                            list.push( Math.pow(n2,n1));
+                            list.push( Math.pow(n2,n1));//n2 power of n1 
                         	 break;
                     
                          case '%':
@@ -196,27 +206,37 @@ String number="";
     {
     }
     }
-    res = Double.parseDouble(("" + list.pop()));
+    res = Double.parseDouble(("" + list.pop()));//pop the result from the stack
     return res;
 }
 	
-//determined if the final result is int or float number
+
 	
-		public static String root (String n)
+	
+	
+	
+	
+	
+	
+		public static String SpetialCases (String n)
 	{
 		String s=n;
 
 		
 
-		s=s.replaceAll(" ", "");
+		s=s.replaceAll(" ", "");//remove the space
+		
 		char []cr=s.toCharArray();
-
+		
+		
+		
+		//determined the minus numbers 
 		for(int i=1;i<s.length();i++)
 		{
-			if(i==1&&cr[i-1]=='-')
+			if(i==1&&cr[i-1]=='-')//if - in the first expression ,  then replace - by #
 				cr[i-1]='#';
 				
-			if(isOperator(cr[i-1])||cr[i-1]=='(')
+			if(isOperator(cr[i-1])||cr[i-1]=='(') //if an operator follow by -  , then replace - by #
 			{
 				if(cr[i]=='-')
 					cr[i]='#';
@@ -230,7 +250,7 @@ String number="";
 		int indexOf=0;
 		char []ccr=s.toCharArray();
 
-		for(int i=1;i<s.length();i++)
+		for(int i=1;i<s.length();i++)//to handle the cases of 7√2 ,(2*3)2 , 2(5-1), 5 !4 , 3 log(10) and 5 ln8 for Example.
 		{if((isOperand(ccr[i-1])&&(ccr[i]=='√'||ccr[i]=='('))||
 				(isOperand(ccr[i])&&ccr[i-1]==')')||
 				(ccr[i-1]==')'&&ccr[i]=='(')||isOperand(ccr[i-1])
@@ -263,17 +283,29 @@ String number="";
 			full=s;
 		
 		String x="";
-		x=full.replace("ln","l");
-		x=full.replace("log","g");
 		
+		x=full.replace("ln","l");//replace ln to l in Expression
+		x=full.replace("log","g");//replace log to g in Expression
 
- return x;
+				
+				full=x.replace("#*(", "#1*(");//to handle the "-()"
+				full=full.replace("#*l", "#1*l");//to handle the "-ln"
+				full=full.replace("#*g", "#1*g");//to handle the "-log()"
+				full=full.replace("#*!", "#1*!");//to handle the "-!num "
+				return full;
+		
+		
+		
+			
         }
+		
+		
+		//calculate the factorial for a number
 	 public static double fact(double c )throws Exception
 		{
 		
 			 if(c<0){throw new Exception("sss");}
-		 double facres;
+			 double facres;
 			if(c==1)
 			{
 				return 1;
@@ -282,6 +314,13 @@ String number="";
 			return facres;
 		
 		}
+	 
+	
+	 
+	 
+	 
+	 
+	//determined if the output is floating number or integer number 
  public static String result(double n)
 	{
 		int b= (int)n;
@@ -293,3 +332,5 @@ String number="";
 				
 	}
 }
+
+
