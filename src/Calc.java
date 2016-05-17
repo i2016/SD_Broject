5370795f0a91fb88c6f6d4d20671685e30915008
Ahmed1othman <othmanahmed865@gmail.com>
2016-05-17 09:54:55 +0200

    avoid duplicate

---
+++ src/Calc.java
@@ -0,0 +1,515 @@
+
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.geometry.Insets;
+import javafx.scene.Scene;
+import javafx.scene.control.*;
+import javafx.stage.*;
+import javafx.scene.layout.*;
+/**
+ *
+ * @author AHMED SERAG
+ */
+public class Calc extends Application 
+{
+    GridPane pane =new GridPane();
+	TextField TextCal=new TextField();
+	Operation operation;
+	boolean []clear=new boolean[1]; // text field clear when user write new number 
+	/* Button Creation For All*/
+        Button button0=new Button("0");
+	Button button1=new Button("1");
+	Button button2=new Button("2");
+	Button button3=new Button("3");
+	Button button4=new Button("4");
+	Button button5=new Button("5");
+	Button button6=new Button("6");
+	Button button7=new Button("7");
+	Button button8=new Button("8");
+	Button button9=new Button("9");
+	Button buttondot=new Button(".");//button for Dot eg 6.4
+	Button buttonClearOne=new Button("C");//button clear for all in TextField
+	Button buttonplus=new Button("+");
+	Button buttonmiuns=new Button("-");
+	Button buttonmulti=new Button("*");
+	Button buttondiv=new Button("/");
+	Button buttonmod=new Button("%");
+	Button buttonpeq=new Button("=");
+	Button buttonpow=new Button("X^n");//for any power 
+	Button buttonroot=new Button("√");//button for find root
+	Button buttonClearAll=new Button("AC");//button clear by one
+	Button buttonleftarc=new Button("(");
+	Button buttonrightarc=new Button(")");
+	Button buttonminusNumber=new Button("+/-");//***** for minus number which use in operations ******
+        Button buttonlog=new Button("log(");
+	Button buttonln=new Button("ln(");
+	Button buttonfact=new Button("!X");
+        Button buttonesc=new Button("ESC");
+    @Override
+    public void start(Stage stage) 
+            
+    { 
+
+/////// Put   Minimum Width For Button
+button1.setMinWidth(70);button7.setMinWidth(70);buttonplus.setMinWidth(70);
+button2.setMinWidth(70);button8.setMinWidth(70);buttonmiuns.setMinWidth(70);
+button3.setMinWidth(70);button9.setMinWidth(70);buttonmulti.setMinWidth(70);
+button4.setMinWidth(70);button0.setMinWidth(70);buttondiv.setMinWidth(70);
+button5.setMinWidth(70); buttondot.setMinWidth(70);buttonmod.setMinWidth(70);
+button6.setMinWidth(70); buttonClearAll.setMinWidth(70);buttonpeq.setMinWidth(70);
+buttonroot.setMinWidth(70); buttonClearOne.setMinWidth(70);
+buttonpow.setMinWidth(70);buttonleftarc.setMinWidth(70);buttonrightarc.setMinWidth(70);
+buttonminusNumber.setMinWidth(70);buttonlog.setMinWidth(70);buttonln.setMinWidth(70);
+buttonfact.setMinWidth(70);buttonesc.setMinWidth(70);
+button1.setPrefHeight(50);button7.setPrefHeight(50);buttonplus.setPrefHeight(50);
+button2.setPrefHeight(50);button8.setPrefHeight(50);buttonmiuns.setPrefHeight(50);
+button3.setPrefHeight(50);button9.setPrefHeight(50);buttonmulti.setPrefHeight(50);
+button4.setPrefHeight(50);button0.setPrefHeight(50);buttondiv.setPrefHeight(50);
+button5.setPrefHeight(50); buttondot.setPrefHeight(50);buttonmod.setPrefHeight(50);
+button6.setPrefHeight(50); buttonClearAll.setPrefHeight(50);buttonpeq.setPrefHeight(50);
+buttonpeq.setPrefHeight(50);buttonroot.setPrefHeight(50); buttonClearOne.setPrefHeight(50);
+buttonpow.setPrefHeight(50);buttonleftarc.setPrefHeight(50);buttonrightarc.setPrefHeight(50);
+buttonminusNumber.setPrefHeight(50);buttonfact.setMinHeight(50);
+buttonlog.setPrefHeight(50);buttonln.setPrefHeight(50);
+TextCal.setPrefHeight(80);buttonesc.setPrefHeight(50);
+
+
+
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
+pane.add(TextCal,0,0,5,1);
+pane.setPadding(new Insets(20,20,20,20));
+pane.setHgap(5);
+pane.setVgap(5);
+//////////////////PUT BUTTON IN PANE////////
+pane.addRow(2,buttonpow,buttonmod,buttonClearAll,buttonClearOne); 
+pane.addRow(3,button1,button2,button3,buttonplus);    
+pane.addRow(4,button4,button5, button6,buttonmiuns);    
+pane.addRow(5,button7,button8, button9,buttonmulti);
+pane.addRow(6,button0, buttondot, buttonminusNumber,buttondiv); 
+pane.addRow(7,buttonroot,buttonlog,buttonfact,buttonln);
+pane.addRow(8,buttonpeq,buttonleftarc,buttonrightarc,buttonesc);
+TextCal.setEditable(false); //for prevent to write from the keybord
+//Event For All Button 
+button0.setOnAction(e->Funzero()); /////// Action in funcation
+button1.setOnAction(e->Funone());
+button2.setOnAction(e->Funtwo());
+button3.setOnAction(e->Funthree());
+button4.setOnAction(e->Funfour());
+button5.setOnAction(e->Funfive());
+button6.setOnAction(e->Funsix());
+button7.setOnAction(e->Funseven());
+button8.setOnAction(e->Funeight());
+button9.setOnAction(e->Funnine());
+ buttonClearAll.setOnAction(e->Funclear());
+buttonplus.setOnAction(e->Funplus());
+buttonmiuns.setOnAction(e->Funmiuns());
+buttonmulti.setOnAction(e->Funmul());
+buttondiv.setOnAction(e->Fundiv());
+ buttonpow.setOnAction(e->Funpow());
+ buttondot.setOnAction(e->Fundot());
+buttonmod.setOnAction(e->Funreminder());
+buttonleftarc.setOnAction(e->Funarcleft());
+buttonrightarc.setOnAction(e->Funarcright());
+buttonpeq.setOnAction(e->Funequal1());
+buttonroot.setOnAction(e->Funroot());
+ buttonClearOne.setOnAction(e->Funclearone());
+buttonminusNumber.setOnAction(e->Funmiunsnumber());// for miuns number
+buttonlog.setOnAction(e->Funlog());
+buttonln.setOnAction(e->Funln());
+buttonfact.setOnAction(e->Funfact());
+buttonesc.setOnAction(new EventHandler<ActionEvent>(){
+					@Override
+					public void handle(ActionEvent e)
+					{ 
+						stage.close();
+						
+						}});
+
+
+
+
+///////CSS Stayle///////
+pane.setStyle("-fx-font-size:20;-fx-background-color:#000000; ");
+buttonClearOne.setStyle("-fx-background-color:#393939;;-fx-text-fill: white;");
+buttonClearAll.setStyle("-fx-background-color:#393939;-fx-text-fill: white;");
+TextCal.setStyle("-fx-background-color:#white;-fx-text-fill:black;-fx-border-width:5px;-fx-border-color:gray;");
+buttonplus.setStyle("-fx-background-color: #393939;-fx-text-fill: white;");
+buttonmiuns.setStyle("-fx-background-color: #393939;-fx-text-fill: white;");
+
+buttonmulti.setStyle("-fx-background-color: #393939;-fx-text-fill: white;");
+buttondiv.setStyle("-fx-background-color: #393939;-fx-text-fill: white;");
+buttonpow.setStyle("-fx-background-color: #393939;-fx-text-fill: white;");
+buttonroot.setStyle("-fx-background-color: #393939;-fx-text-fill: white;");
+buttonlog.setStyle("-fx-background-color: #393939;-fx-text-fill: white;");
+buttonln.setStyle("-fx-background-color: #393939;-fx-text-fill: white;");
+buttonfact.setStyle("-fx-background-color: #393939;-fx-text-fill: white;");
+buttonmod.setStyle("-fx-background-color: #393939;-fx-text-fill: white;");
+buttonleftarc.setStyle("-fx-background-color:#393939;-fx-text-fill: white;");
+buttonrightarc.setStyle("-fx-background-color:#393939;-fx-text-fill: white;");
+buttonminusNumber.setStyle("-fx-background-color: #393939;-fx-text-fill: white;");
+buttonpeq.setStyle("-fx-background-color:#339933;-fx-text-fill: white;");
+buttonesc.setStyle("-fx-background-color:#393939;-fx-text-fill: red;");
+button0.setStyle("-fx-background-color:#1f1f1f;-fx-text-fill: white;");
+button1.setStyle("-fx-background-color:#1f1f1f;-fx-text-fill: white;");
+button2.setStyle("-fx-background-color:#1f1f1f;-fx-text-fill: white;");
+button3.setStyle("-fx-background-color:#1f1f1f;-fx-text-fill: white;");
+button4.setStyle("-fx-background-color:#1f1f1f;-fx-text-fill: white;");
+button5.setStyle("-fx-background-color:#1f1f1f;-fx-text-fill: white;");
+button6.setStyle("-fx-background-color:#1f1f1f;-fx-text-fill: white;");
+button7.setStyle("-fx-background-color:#1f1f1f;-fx-text-fill: white;");
+button8.setStyle("-fx-background-color:#1f1f1f;-fx-text-fill: white;");
+button9.setStyle("-fx-background-color:#1f1f1f;-fx-text-fill: white;");
+buttondot.setStyle("-fx-background-color:#1f1f1f;-fx-text-fill: white;");
+ 
+//Showing  Scene 
+Scene scene=new Scene(pane);
+stage.setTitle("Calculator");
+stage.setScene(scene);
+stage.show();
+    }
+    /////////// handle action for All Event
+public void Funzero()
+   {
+    	  if(clear[0]==true) // if user press equel and clear[0]==true TextCal will Be cleared
+      	{
+      		TextCal.clear();
+      		
+      	}
+      	clear[0]=false; // if  user didn't press equel and clear[0] not equel true TextCal will be write ites Action
+String s= (TextCal.getText()+"0");
+TextCal.setText(s);
+   }
+   
+  
+public void Funone()
+    {	
+    	if(clear[0]==true) // if user press equel and clear[0]==true TextCal will Be cleared
+    	{
+    		TextCal.clear();
+    		
+    	}
+    	clear[0]=false;// if  user didn't press equel and clear[0] not equel true TextCal will be write ites Action
+	String s= (TextCal.getText()+"1");
+TextCal.setText(s);
+    }
+public void Funtwo()
+    {	
+       if(clear[0]==true)// if user press equel and clear[0]==true TextCal will Be cleared
+    	{
+    		TextCal.clear();
+    		
+    	}
+    	clear[0]=false;// if  user didn't press equel and clear[0] not equel true TextCal will be write ites Action
+	String s= (TextCal.getText()+"2");
+TextCal.setText(s);
+    }
+public void Funthree() // if user press equel and clear[0]==true TextCal will Be cleared
+    {	
+    	if(clear[0]==true)
+    	{
+    		TextCal.clear();
+    		
+    	}
+    	clear[0]=false;// if  user didn't press equel and clear[0] not equel true TextCal will be write ites Action
+	String s= (TextCal.getText()+"3");
+TextCal.setText(s);}
+public void Funfour()
+    {	
+    	if(clear[0]==true) // if user press equel and clear[0]==true TextCal will Be cleared
+    	{
+    		TextCal.clear();
+    		
+    	}
+    	clear[0]=false; // if  user didn't press equel and clear[0] not equel true TextCal will be write ites Action
+	String s= (TextCal.getText()+"4");
+TextCal.setText(s);
+    }
+public void Funfive()
+    {	
+    	if(clear[0]==true)// if user press equel and clear[0]==true TextCal will Be cleared
+    	{
+    		TextCal.clear();
+    		
+    	}
+    	clear[0]=false; // if  user didn't press equel and clear[0] not equel true TextCal will be write ites Action
+	String s= (TextCal.getText()+"5");
+TextCal.setText(s);
+    }
+public void Funsix()
+    {	if(clear[0]==true)// if user press equel and clear[0]==true TextCal will Be cleared
+	{
+		TextCal.clear();
+		
+	}
+	clear[0]=false; // if  user didn't press equel and clear[0] not equel true TextCal will be write ites Action
+	String s= (TextCal.getText()+"6");
+TextCal.setText(s);
+    }
+ public void Funseven()
+    {	
+    	if(clear[0]==true) // if user press equel and clear[0]==true TextCal will Be cleared
+    	{
+    		TextCal.clear();
+    		
+    	}
+    	clear[0]=false; // if  user didn't press equel and clear[0] not equel true TextCal will be write ites Action
+	String s= (TextCal.getText()+"7");
+TextCal.setText(s);
+    }
+public void Funeight()
+    {	if(clear[0]==true)// if user press equel and clear[0]==true TextCal will Be cleared
+	{
+		TextCal.clear();
+		
+	}
+	clear[0]=false; // if  user didn't press equel and clear[0] not equel true TextCal will be write ites Action
+	String s= (TextCal.getText()+"8");
+TextCal.setText(s);
+    }
+    public void Funnine() 
+    {	
+    	if(clear[0]==true)// if user press equel and clear[0]==true TextCal will Be cleared
+    	{
+    		TextCal.clear();
+    		
+    	}
+    	clear[0]=false; // if  user didn't press equel and clear[0] not equel true TextCal will be write ites Action
+	String s= (TextCal.getText()+"9");
+TextCal.setText(s);
+    }
+    public void Funmiuns()
+    {	
+	String s=TextCal.getText();
+	if(Operation.duplicateOperator(s, '-'))
+	{
+	 s= (TextCal.getText());
+	}else
+	{
+	 s= (TextCal.getText()+" - ");
+	}
+TextCal.setText(s);
+clear[0]=false;
+    }
+public void Funplus()
+     {	
+		
+	String s=TextCal.getText();
+	if(Operation.duplicateOperator(s, '+'))
+	{
+	 s= (TextCal.getText());
+	}else
+	{
+	 s= (TextCal.getText()+" + ");
+	}
+TextCal.setText(s);
+clear[0]=false;
+      }
+public void Funmul()
+    {	
+	String s=TextCal.getText();
+	if(Operation.duplicateOperator(s, '*'))
+	{
+	 s= (TextCal.getText());
+	}else
+	{
+	 s= (TextCal.getText()+" * ");
+	}
+TextCal.setText(s);
+        clear[0]=false;
+    }
+public void Fundiv()
+    {	
+	String s=TextCal.getText();
+	if(Operation.duplicateOperator(s, '/'))
+	{
+	 s= (TextCal.getText());
+	}else
+	{
+	 s= (TextCal.getText()+" / ");
+	}
+TextCal.setText(s);
+clear[0]=false;
+    }
+public void Fundot()
+    {
+	String s=TextCal.getText();
+	if(Operation.duplicateDot(s, "."))
+	{
+	 s= (TextCal.getText());
+	}else
+	{
+	 s= (TextCal.getText()+".");
+	}
+TextCal.setText(s);
+clear[0]=false;
+    }
+public void Funpow()
+    {	
+   
+	String s=TextCal.getText();
+	if(Operation.duplicateOperator(s, '^'))
+	{
+	 s= (TextCal.getText());
+	}else
+	{
+	 s= (TextCal.getText()+"^");
+	}
+TextCal.setText(s);
+    	clear[0]=false;
+     }
+public void Funreminder()
+    {String s=TextCal.getText();
+	if(Operation.duplicateOperator(s, '%'))
+	{
+	 s= (TextCal.getText());
+	}else
+	{
+	 s= (TextCal.getText()+"% ");
+	}
+TextCal.setText(s);
+    	clear[0]=false;
+    }
+public void Funarcleft()
+    { 
+        if(clear[0]==true)
+	{
+		TextCal.clear();
+		
+	}
+	clear[0]=false;
+    	String s= (TextCal.getText()+" (");
+    	TextCal.setText(s);
+    	
+    }
+public void Funarcright()
+    { 
+        if(clear[0]==true)
+	{
+		TextCal.clear();
+		
+	}
+	clear[0]=false;
+    	String s= (TextCal.getText()+") ");
+    	TextCal.setText(s);
+    }
+    
+public void Funroot()
+    {
+    	String s= (TextCal.getText()+"√");
+       	TextCal.setText(s);
+       	clear[0]=false;
+      }
+public void Funequal1()
+ {	
+    	clear[0]=false;
+    	try
+        {
+String s=TextCal.getText();
+String cal=""+Operation.toPost(Operation.SpetialCases(s));
+TextCal.setText(cal);
+clear[0]=true;
+       }
+    	catch(Exception e)
+        {
+    	TextCal.setText("Error");
+    	clear[0]=true;
+    	}
+
+}
+public void Funmiunsnumber() ///////// minus Button
+    {
+	String s=TextCal.getText();
+	if(Operation.duplicateMinus(s, "-"))
+	{
+	 s= (TextCal.getText());
+	}else
+	{
+	 s= (TextCal.getText()+"-");
+	}
+TextCal.setText(s);
+    	clear[0]=false;
+    	
+    }
+
+public void Funclear()
+{	
+
+TextCal.clear(); /////////////////////clear if text is empty do nothing
+}
+public void Funclearone() 
+{
+	try             //deleting by One
+        {
+	TextCal.deleteText(TextCal.getText().length()-1, TextCal.getText().length());
+        }
+         catch(Exception s)
+        {
+	TextCal.setText(" ");
+        }
+}
+public void Funlog()
+{
+ 	  if(clear[0]==true) // if  user press equel and clear[0] equel true TextCal will be write ites Action
+   	{
+   		TextCal.clear();
+   		
+   	}
+   	clear[0]=false;// if  user didn't press equel and clear[0] not equel true TextCal will be write ites Action
+String s= (TextCal.getText()+"log(");
+TextCal.setText(s);
+}
+
+public void Funln()
+{
+ 	  if(clear[0]==true) // if  user press equel and clear[0] equel true TextCal will be write ites Action
+   	{
+   		TextCal.clear();
+   		
+   	}
+   	clear[0]=false;// if  user didn't press equel and clear[0] not equel true TextCal will be write ites Action
+String s= (TextCal.getText()+"ln(");
+TextCal.setText(s);
+}
+public void Funfact()
+{
+ 	  if(clear[0]==true) // if  user press equel and clear[0] equel true TextCal will be write ites Action
+   	{
+   		TextCal.clear();
+   		
+   	}
+   	clear[0]=false;// if  user didn't press equel and clear[0] not equel true TextCal will be write ites Action
+String s= (TextCal.getText()+"!");
+TextCal.setText(s);
+}
+public void Funequal()
+ {	
+    	clear[0]=false;
+    	try
+        {
+String s=TextCal.getText();   //if user pres equal button it will to topost funcaton after excueting root Funcation 
+String cal=""+Operation.toPost(Operation.SpetialCases(s));
+TextCal.setText(cal);
+clear[0]=true;
+       }
+    	catch(Exception e)
+        {
+            if(clear[0]==false)// if user press equel and clear[0]==true TextCal will Be cleared
+    	{
+    		TextCal.clear();
+    		
+    	}
+    	
+    	TextCal.setText("Error");
+    	clear[0]=true;
+    	}
+
+}
+
+    public static void main(String[] args) 
+    {
+        launch(args);
+    }
+  
+}
---
+++ src/Operation.java
@@ -0,0 +1,404 @@
+
+
+
+
+import java.util.Stack;
+public class Operation 
+{
+    static Stack<Double> list = new Stack<Double>();// use to evaluation postfix Expression
+    
+	// Determine if character is number or not 
+public static <E> boolean isOperand(E ch)
+	{
+	 if(ch.equals('.')||ch.equals('0')||ch.equals('1')||ch.equals('2')||ch.equals('3')||ch.equals('4')||ch.equals('5')||ch.equals('6')||ch.equals('7')||ch.equals('8')||ch.equals('9')||ch.equals('#'))
+	 return true;
+	 else 
+         return false;
+	 
+	}
+// Determine if character is Operator or not  
+public static <E> boolean isOperator(E ch)
+	{
+          if(ch.equals('=')||ch.equals('+')||ch.equals('-')||ch.equals('/')||ch.equals('*')||ch.equals('^')||ch.equals('%')||ch.equals('√')||ch.equals('!')||ch.equals('l')||ch.equals('g'))
+	 return true;
+	 else 
+         return false;
+	 }
+	// Determine the priority of operator
+public static <E> int prority(E ch)
+	{
+		
+		switch ((Character)ch)
+		{
+		case'+':case'-': return 1;
+		
+		case'*':case'/':case'%': return 2;
+		
+		case'^':case'√': case'!':case'l':case'g':return 3;
+		
+		default :return 0;
+		}
+	}
+
+
+public static String toPost(String inp) throws Exception
+       {
+		String result="";       //Create Empty Postfix String
+		String copy='('+inp+')';  //Given Infix To Store String
+		Stack<Character> operator=new Stack<>();/*Create Generic Stack To Store 
+                Operator And Organize Priority Of Operators */
+		int size=copy.length();//size of operation
+		char ch,chh;
+		for(int i=1;i<size;i++)
+		{                             /* if ch is an operand then
+                                              append it to the output postfix string */
+			
+			ch=copy.charAt(i-1);
+			chh=copy.charAt(i);
+			if(isOperand(ch))
+			{                             /*if ch is an operand then
+                                              append it to the output postfix string */
+				if(isOperator(chh)||chh==')'||chh==')')//to determined the full number and the end of number
+				result+=ch+" ";  //add the number flowing by space to separate the elements
+				else result+=ch;//if number still complete it
+			}
+		
+                        else if(isOperator(ch))   //if element is operator then check if :
+                        {  
+				if(!operator.empty())       //the stack is not empty
+					{
+					while(!operator.empty())  
+                               /* while operator stack not empty ,pop operator
+                                    append it to the postfix string*/
+                                     {        
+						if(prority(ch)>prority((Character)operator.peek())) //determined the priority of operand
+								{
+								operator.push(ch);
+								break;
+								}
+						else
+							result+=operator.pop( )+" ";
+					}
+					}
+				else 
+			    operator.push(ch);
+			}
+			else if(ch=='('){
+				operator.push(ch);}
+			else if(ch==')')//end of braces 
+			{
+				while((Character)operator.peek()!='(')
+				{
+					result+=operator.pop()+" ";
+				}
+				operator.pop();
+			}
+			
+			}
+			while(operator.size()>1)
+				result+=operator.pop();       // to make sure that no thing in stack
+			
+		return result(evalPost(result));//evalPost(result.split(" ")); 
+	}
+
+
+
+
+
+//compute the postfix expression 
+	public static double evalPost(String exp) throws Exception
+	{
+	   double res = 0;//final result
+
+    double n1;     //result of 1st popping
+    double n2;     // result of 2nd popping
+    char ch = 0 ;
+    String number="";
+    for (int i = 1; i <= exp.length(); i++) {
+         ch = exp.charAt(i-1);
+
+
+            if (isOperand(ch)) {
+            	if(isOperand(exp.charAt(i)))
+            	{
+            		number+=ch;//collect full number
+           		}
+            	else 
+            	{
+            		number+=ch;
+            		String s= number.replace("#", "-");//replace the # by minus if found 
+            		list.push (Double.parseDouble(s));
+            		number="";
+            		s="";
+            	}
+            		
+            	
+            }
+             
+    else if (isOperator(ch)) //if operator do the operation on number according to the kind of it
+            
+            	 {   if(ch=='!'||ch=='√'||ch=='l'||ch=='g') //operations that have single pop number
+                 {
+    	    n1 = Double.parseDouble(("" + list.pop()));
+                    switch (ch) {  
+            	 case '√':
+                   {
+                    	 list.push(Math.sqrt(n1));
+                    	 break;
+                   }
+                   
+            	
+            	 case 'l':
+                 {
+                  	 list.push(Math.log((n1)));
+                  	break;
+                 }
+            	 case 'g':
+                 {
+                  	 list.push(Math.log10(n1));
+                  	break;
+                 }
+            	 case '!':
+            	
+            	 {	
+            		  list.push(fact(n1));break;
+                 }
+                 }
+            	 }
+                   
+            	 else
+            		{
+            		 //operations that have two pop number
+            		 n1 = Double.parseDouble(("" + list.pop()));
+                          n2 = Double.parseDouble(("" + list.pop()));
+                     switch (ch) 
+                     {
+                         case '+':
+                             list.push(n1 + n2);//adding
+                             break;
+                         case '-':
+                             list.push(n2 - n1);//Subtracting
+                             break;
+                         case '*':
+                             list.push(n1 * n2);//Multiply
+                             break;
+                         case '/':
+                             try{
+                            	 
+                        	 list.push(n2 / n1);//Division
+                             }catch(Exception ex)
+                             {
+                                                         
+                             }   break;
+                         case '^':
+                            list.push( Math.pow(n2,n1));//n2 power of n1 
+                        	 break;
+                    
+                         case '%':
+                        	 list.push(n2 % n1);
+                            	 break;
+
+                         default:
+                             System.out.println("Invalid operator order!");
+                     }  }
+            	 }
+    else
+    {
+    }
+    }
+    res = Double.parseDouble(("" + list.pop()));//pop the result from the stack
+    return res;
+}
+	
+
+	
+	
+	
+	
+	
+	
+	
+		public static String SpetialCases (String n)
+	{
+		String s=n;
+
+		
+
+		s=s.replaceAll(" ", "");//remove the space
+		
+		char []cr=s.toCharArray();
+		
+		
+		
+		//determined the minus numbers 
+		for(int i=1;i<s.length();i++)
+		{
+			if(i==1&&cr[i-1]=='-')//if - in the first expression ,  then replace - by #
+				cr[i-1]='#';
+				
+			if(isOperator(cr[i-1])||cr[i-1]=='(') //if an operator follow by -  , then replace - by #
+			{
+				if(cr[i]=='-')
+					cr[i]='#';
+			}
+		}
+		s="";
+		for (Character c : cr)
+		    s += c.toString();
+		int[] index=new int[s.length()];
+		index[0]=0;
+		int indexOf=0;
+		char []ccr=s.toCharArray();
+
+		for(int i=1;i<s.length();i++)//to handle the cases of 7√2 ,(2*3)2 , 2(5-1), 5 !4 , 3 log(10) and 5 ln8 for Example.
+		{if((isOperand(ccr[i-1])&&(ccr[i]=='√'||ccr[i]=='('))||
+				(isOperand(ccr[i])&&ccr[i-1]==')')||
+				(ccr[i-1]==')'&&ccr[i]=='(')||isOperand(ccr[i-1])
+				&&(ccr[i]=='!')||isOperand(ccr[i-1])&&(ccr[i]=='g')
+				||isOperand(ccr[i-1])&&(ccr[i]=='l'))
+		index[indexOf++]=i;
+			
+		}
+		indexOf=0;
+		String full="";
+		if(index[indexOf]!=0){
+		for(int i=0;i<ccr.length;i++)
+		{
+			if(i==index[indexOf])
+			{
+				if(ccr[i]==')'){
+				full+=ccr[i]+"*";
+				indexOf++;}
+				else
+				{
+					full+="*"+ccr[i];
+					indexOf++;
+					}
+			}
+			else
+				full+=ccr[i];
+		}
+		}
+		else
+			full=s;
+		
+		String x="";
+		
+		x=full.replace("ln","l");//replace ln to l in Expression
+		x=full.replace("log","g");//replace log to g in Expression
+
+				
+				full=x.replace("#*(", "#1*(");//to handle the "-()"
+				full=full.replace("#*l", "#1*l");//to handle the "-ln"
+				full=full.replace("#*g", "#1*g");//to handle the "-log()"
+				full=full.replace("#*!", "#1*!");
+				full=full.replace(")√", ")*√");//to handle the "-!num "
+				return full;
+		
+		
+		
+			
+        }
+		
+		
+		//calculate the factorial for a number
+	 public static double fact(double c )throws Exception
+		{
+		
+			 if(c<0){throw new Exception("sss");}
+			 double facres;
+			if(c==1)
+			{
+				return 1;
+			}
+			facres=fact(c-1)*c;
+			return facres;
+		
+		}
+	 
+	
+	 
+	 
+	 
+	 
+	//determined if the output is floating number or integer number 
+ public static String result(double n)
+	{
+		int b= (int)n;
+		double v=(n-b);
+				if(v==0)
+					return String.valueOf(b);
+				else
+					return String.valueOf(n);
+				
+	}
+
+
+
+
+
+
+
+
+//to avoid duplicate dot
+ public static boolean duplicateDot(String n,String c)
+ {
+	 if(n.length()==0)
+	 {
+		 return false;
+	 }
+	String []d=n.split(" ");
+	String f=d[d.length-1];
+	 if(f.contains(c))
+	 {
+		 return true;
+	 }
+	 else
+		 return false;
+ }
+ 
+ //to avoid duplicate operator
+ public static boolean duplicateOperator(String n,char c)
+ {
+
+	 if(n.length()==0)
+	 {
+		 return true;
+	 }
+		String []d=n.split(" ");
+		String f=d[d.length-1];
+		if(f.charAt(f.length()-1)=='+'||f.charAt(f.length()-1)=='/'||
+				f.charAt(f.length()-1)=='*'||f.charAt(f.length()-1)=='^'||
+				f.charAt(f.length()-1)=='%'||f.charAt(f.length()-1)=='-')
+		{
+			return true;
+		}
+		else
+			return false;
+	 
+ }
+ 
+ 
+ //avoid duplicate of minus
+ public static boolean duplicateMinus(String n,String c)
+ {
+	 if(n.length()==0)
+	 {
+		 return false;
+	 }
+	 if(n.charAt(n.length()-1)==' ')
+	 {
+		 return false;
+	 }
+	 else{
+	String []d=n.split(" ");
+	String f=d[d.length-1];
+	 if(f.contains(c))
+	 {
+		 return true;
+	 }
+	 else
+		 return false;}
+ }
+ 
+}
